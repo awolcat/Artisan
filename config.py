6b8c1e6028e557cf4a79345d52427ded5ded1584
Habeeb Dindi <habeebdindi@inspiron-7405-2n1.lan>
2023-10-23 23:48:09 +0100

    Add config file for flask-sqlalchemy db

---
+++ config.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+"""COnfigures variables for SQLAlchemy
+"""
+import os
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql://root:dell2023@localhost/app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
