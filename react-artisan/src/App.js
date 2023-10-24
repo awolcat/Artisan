5de92316940c238a98355315248ae7fe7d10422d
Albert <albertirura11@gmail.com>
2023-10-24 09:30:01 +0300

    added more routes and styles

--- react-artisan/src/App.js
+++ react-artisan/src/App.js
@@ -3,9 +3,12 @@ import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 // import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import About from './components/pages/about';
 import Landing from './components/pages/landing';
 import Contractors from './components/pages/contractors';
+import Jobs from './components/pages/jobs';
+
 
 function App() {
   return (
@@ -14,9 +17,10 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Landing />} />
         <Route path='/about' element={<About />} />
-        
+        <Route path='/jobs' element={<Jobs />} />
         <Route path='/contractors' element={<Contractors />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
