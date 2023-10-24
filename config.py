7052f5cf33dae19fe35bd01a50c162a128ea8f07
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-10-24 21:40:44 +0100

    Update config file

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ import os
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://root:artisan_db_pwd@localhost/artisan'
+        'mysql://root:artisan_db_pwd@localhost/artisan_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
