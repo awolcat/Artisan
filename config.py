c05cf16b74abecd96d0f97ec12d5baead47a684e
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-10-26 06:14:29 +0100

    Correct comment

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-#/usr/bin/python3
-"""COnfigures variables for SQLAlchemy
+# /usr/bin/python3
+"""Configures variables for SQLAlchemy
 """
 import os
 
@@ -8,4 +8,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'mysql://root:artisan_db_pwd@localhost/artisan_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
