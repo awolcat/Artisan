1dbf16929a888cd6a5a0bda2932fc780e9212f17
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-10-26 04:57:35 +0100

    Create first version of api

---
+++ flask_app/api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Create a blueprint
+"""
+from flask import Blueprint
+
+api_views = Blueprint('api_views', __name__, url_prefix='/api/v1')
+
+from flask_app.api.v1.views.contractors import *
---
+++ flask_app/api/v1/views/contractors.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+"""
+from flask_app.api.v1.views import api_views
+from flask import abort, jsonify, request
+from models.contractors import Contractor
+from models.schemas import ContractorSchema
+
+
+contractor_schema = ContractorSchema()
+contractors_schema = ContractorSchema(many=True)
+
+@api_views.route('/contractors', methods=['GET'])
+def get_contractors():
+    """Returns a dictionary of contractors
+    """
+    all_contractors = Contractor.query.all()
+    contractors = contractors_schema.dump(all_contractors).data
+    return jsonify({"contractors": contractors})
+
