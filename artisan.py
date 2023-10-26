b588525515d2cafa1210b3b3f5e762a6c9e5e5ab
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-10-26 04:53:01 +0100

    Configure flask shell context to run flask shell smoothly

--- artisan.py
+++ artisan.py
@@ -2,3 +2,26 @@
 """
 """
 from flask_app import app
+from flask_app import db, ma
+from models.base import Base
+from models.users import User
+from models.bookings import Booking
+from models.contractors import Contractor
+from models.portfolios import Portfolio
+from models.services import Service
+from models.user_reviews import UserReview
+from models.schemas import *
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db,
+            'ma': ma,
+            'Base': Base,
+            'User': User,
+            'Booking': Booking,
+            'Contractor': Contractor,
+            'Portfolio': Portfolio,
+            'Service': Service,
+            'UserReview': UserReview
+    }
