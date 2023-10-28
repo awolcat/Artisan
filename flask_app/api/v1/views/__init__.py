2853d3114a8ac1c195191a379f7c7153deee2de5
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-10-28 11:39:59 +0100

    Update name space to create sensible urls

--- flask_app/api/v1/views/__init__.py
+++ flask_app/api/v1/views/__init__.py
@@ -4,7 +4,7 @@
 from flask import Blueprint
 from flask_restx import Api
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api')
 api = Api(app_views, doc='/doc', version='1.0', title="Artisan RESTful API")
 
 from flask_app.api.v1.views.contractors import *
