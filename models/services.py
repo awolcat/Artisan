07d5bedefd8b2783a3300670a8d1e4c710a77bcd
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-01 02:17:17 +0100

    Add a new relationship with contract

--- models/services.py
+++ models/services.py
@@ -6,9 +6,13 @@ from models.base import Base
 
 
 class Service(Base, db.Model):
-    """This class defines a service"""
+    """This class defines a service.
+       There is a many-to-many relationship between contractors and services.
+       The relationship and associative table are defined in contractors.py
+    """
     name = db.Column(db.String(45), nullable=False)
     description = db.Column(db.Text)
+    contracts = db.relationship('Contract', backref='service', lazy='dynamic')
 
     def __init__(self, **kwargs):
         """Initialises a service
