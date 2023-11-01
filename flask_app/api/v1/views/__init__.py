3e34ec3aef05d32e2fedc83eedee0f8c4f15b58b
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-01 02:08:48 +0100

    Import all routes from their respective files

--- flask_app/api/v1/views/__init__.py
+++ flask_app/api/v1/views/__init__.py
@@ -6,3 +6,11 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from flask_app.api.v1.views.contractors import *
+from flask_app.api.v1.views.service_offers import *
+from flask_app.api.v1.views.users import *
+from flask_app.api.v1.views.contracts import *
+from flask_app.api.v1.views.portfolios import *
+from flask_app.api.v1.views.user_reviews import *
+from flask_app.api.v1.views.services import *
+from flask_app.api.v1.views.bookings import *
+
