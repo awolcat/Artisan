094b43c37d91953a8c48e791cef1e55a186878ab
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-03 23:11:21 +0100

    Update config file with JWT variables

--- config.py
+++ config.py
@@ -2,9 +2,13 @@
 """Configures variables for SQLAlchemy
 """
 import os
+from datetime import timedelta
 
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'mysql://root:artisan_db_pwd@localhost/artisan_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "ARTISAN_2023_SECRET_TOKEN"
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
