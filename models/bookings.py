b4d9a5b092910d1c9d8a09199f46d9c0ae83b342
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-03 02:41:14 +0100

    Comment out irrelevant fields

--- models/bookings.py
+++ models/bookings.py
@@ -10,13 +10,14 @@ class Booking(Base, db.Model):
     """This class defines a booking"""
     booking_date = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(60))
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     contract_id = db.Column(db.Integer, db.ForeignKey('contract.id'), nullable=True)
-    active_user = db.Column(db.Boolean, default=True)
     contractor_id = db.Column(db.Integer, db.ForeignKey('contractor.id'), nullable=False)
     service_offer_id = db.Column(db.Integer, db.ForeignKey('service_offer.id'), nullable=True)
-    active_contractor = db.Column(db.Boolean, default=True)
-    user_reviews = db.relationship('UserReview', backref='booking', cascade='all, delete')
+    user_reviews = db.relationship('UserReview', uselist=False, backref='booking', cascade='all, delete')
+#    active_contractor = db.Column(db.Boolean, default=True)
+#    active_user = db.Column(db.Boolean, default=True)
      
     def __init__(self, **kwargs):
         """Initialises a booking
