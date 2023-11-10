53b647cd252de503809d8a40485253a6b4cad298
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-10 19:32:29 +0100

    Move model diagram to schemas subdirectory

--- flask_app/login.py
+++ flask_app/login.py
@@ -32,7 +32,6 @@ def login_contractor():
         return jsonify({"message": "Wrong username or password"}), 401
     token = create_access_token(identity=contractor)
     response = jsonify({"access-token": token})
-#    set_access_cookies(response, token)
     return response
 
 
@@ -47,7 +46,6 @@ def login_user():
         return jsonify({"message": "Wrong username or password"}), 401
     token = create_access_token(identity=user)
     response = jsonify({"access-token": token})
-    #set_access_cookies(response, token)
     return response
 
 
