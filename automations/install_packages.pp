94cef1733cc1740afea0fc180d45823070558b45
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-13 00:01:53 +0100

    Add comment, remove sudo in commands

--- automations/install_packages.pp
+++ automations/install_packages.pp
@@ -1,10 +1,12 @@
+# This manifest install some basic packages on an ubuntu server
+
 exec { 'apt-update':
-  command => 'sudo apt-get upgrade',
+  command => 'apt-get upgrade',
   path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
 
 exec { 'apt-upgrade':
-  command => 'sudo apt-get upgrade -y',
+  command => 'apt-get upgrade -y',
   path    => '/usr/bin:/usr/sbin:/bin:/sbin',
   require => Exec['apt-update'],
 }
