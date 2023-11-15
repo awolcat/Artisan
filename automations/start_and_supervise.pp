5060fa5a0b5d0f25d0855cff892d4c736d3d9bd4
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-15 02:21:51 +0000

    Correct the command path

--- automations/start_and_supervise.pp
+++ automations/start_and_supervise.pp
@@ -9,7 +9,7 @@ file { 'supervisor_config':
        mode    => '0755',
        content =>
        "[program:artisan]
-command=/home/habeebdindi/artisan/venv-artisan/bin/gunicorn -b localhost:5000 -w 2 artisan:app
+command=/usr/bin/gunicorn --bind localhost:5000 -w 2 --log-level debug artisan:app
 directory=/home/habeebdindi/artisan
 user=habeebdindi
 autostart=true
@@ -19,13 +19,13 @@ killasgroup=true
 
 [program:react_artisan]
 command=/usr/bin/npm start
-directory=/home/habeebdindi/artisan/react-artisan,
+directory=/home/habeebdindi/artisan/react-artisan
 user=habeebdindi
 autostart=true
 autorestart=true
 stopasgroup=true
 killasgroup=true
-	",
+",
 }
 
 exec { 'reload_config_file':
